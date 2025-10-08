bf40f01259533f8a76d716c5a06caa4518113211
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-10-08 17:13:32 +0200

    improve movement

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-var speed = 650.0
-var acceleration = 750.0
+var max_walk_speed = 100
+var acceleration = 165.0 * 10
 var friction = 3.0
 var gravity = 600.0
 var jump_force = 80.0
@@ -9,16 +9,17 @@ var max_jump_length = 0.25
 
 var jump_counter = 0.0
 
-var left_input = "wasd left"
-var right_input = "wasd right"
-var up_input = "wasd up"
-var down_input = "wasd down"
-var jump_input = "wasd jump"
-var attack_input = "wasd attack"
+@export var player_one = false
+var input_set = "player1"
+
+func _ready() -> void:
+	input_set = "player1" if player_one else "player2"
+	if !player_one:
+		$Sprite.texture = load("res://assets/player2.png")
 
 func _process(delta: float) -> void:
 	var on_ground = is_on_floor()
-	if Input.is_action_pressed(jump_input):
+	if Input.is_action_pressed(input_set + " jump"):
 		if on_ground:
 			velocity.y = -jump_force * 1.25
 			jump_counter = max_jump_length
@@ -30,21 +31,30 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	var on_ground = is_on_floor()
-	var move_dir = Input.get_axis(left_input,right_input)
+	var move_dir = Input.get_axis(input_set + " left",input_set + " right")
 	var active_friction = friction
+	var max_speed = max_walk_speed
 	
 	velocity.y += gravity * delta
 	
-	if move_dir == 0 and on_ground:
+	if on_ground and (move_dir == 0 or (move_dir < 0 and velocity.x > 0) or (move_dir > 0 and velocity.x < 0)):
 		active_friction *= 4.0
+		move_dir *= 2.0
+	if !on_ground:
+		max_speed = 100
+		active_friction = 0
 	
-	var can_move = true
-	if velocity.x < -speed and move_dir < 0:
-		can_move = false
-	elif velocity.x > speed and move_dir > 0:
-		can_move = false
+	if move_dir > 0:
+		$Sprite.flip_h = true
+	elif move_dir < 0:
+		$Sprite.flip_h = false
 	
-	if can_move:
+		
+	if velocity.x - acceleration * delta < -max_speed and move_dir < 0:
+		velocity.x = -max_speed
+	elif velocity.x + acceleration * delta > max_speed and move_dir > 0:
+		velocity.x = max_speed
+	else:
 		velocity.x += move_dir * acceleration * delta
 	
 	velocity.x = lerp(velocity.x,0.0,active_friction*delta)
